3ff9e1fa045422d543c039bf7f96d038eaa3b4a7
yoonju1030 <allrealjgt@gmail.com>
2024-10-14 23:48:31 +0900

    make product view

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,115 +1,35 @@
 <script setup>
-import AnalyticsAward from '@/views/dashboard/AnalyticsAward.vue'
-import AnalyticsBarCharts from '@/views/dashboard/AnalyticsBarCharts.vue'
-import AnalyticsDepositWithdraw from '@/views/dashboard/AnalyticsDepositWithdraw.vue'
-import AnalyticsSalesByCountries from '@/views/dashboard/AnalyticsSalesByCountries.vue'
-import AnalyticsTotalEarning from '@/views/dashboard/AnalyticsTotalEarning.vue'
-import AnalyticsTotalProfitLineCharts from '@/views/dashboard/AnalyticsTotalProfitLineCharts.vue'
-import AnalyticsTransactions from '@/views/dashboard/AnalyticsTransactions.vue'
-import AnalyticsUserTable from '@/views/dashboard/AnalyticsUserTable.vue'
-import AnalyticsWeeklyOverview from '@/views/dashboard/AnalyticsWeeklyOverview.vue'
-import CardStatisticsVertical from '@core/components/cards/CardStatisticsVertical.vue'
+import { useRouter } from "vue-router";
+import productMiniVue from './productMini.vue';
+const router = useRouter()
+const productList = [
+    "MDM01", "MDM02", "MDM03", 
+    "MDM04", "MDM05", "MDM06", 
+    "MDM07", "MDM08", "MDM09", 
+    "MDM10", "MDM11", "MDM12"
+]
 
-const totalProfit = {
-  title: 'Total Profit',
-  color: 'secondary',
-  icon: 'ri-pie-chart-2-line',
-  stats: '$25.6k',
-  change: 42,
-  subtitle: 'Weekly Project',
+const clickProduct = (id) => {
+    router.push({path: `/product/${id}`})
 }
 
-const newProject = {
-  title: 'New Project',
-  color: 'primary',
-  icon: 'ri-file-word-2-line',
-  stats: '862',
-  change: -18,
-  subtitle: 'Yearly Project',
-}
 </script>
 
-<template>
-  <VRow class="match-height">
-    <VCol
-      cols="12"
-      md="4"
-    >
-      <AnalyticsAward />
-    </VCol>
+<template> 
+    <h1>
+        상품 소개
+    </h1>
+    <br/>
 
-    <VCol
-      cols="12"
-      md="8"
-    >
-      <AnalyticsTransactions />
-    </VCol>
-
-    <VCol
-      cols="12"
-      md="4"
-    >
-      <AnalyticsWeeklyOverview />
-    </VCol>
-
-    <VCol
-      cols="12"
-      md="4"
-    >
-      <AnalyticsTotalEarning />
-    </VCol>
-
-    <VCol
-      cols="12"
-      md="4"
-    >
-      <VRow class="match-height">
+    <VRow class="match-height">
         <VCol
-          cols="12"
-          sm="6"
+            cols="12"
+            sm="2"
+            v-for="id in productList"
+            :key="id"
+            @click="clickProduct(id)"
         >
-          <AnalyticsTotalProfitLineCharts />
+            <productMiniVue v-bind:productId="id"/>
         </VCol>
-
-        <VCol
-          cols="12"
-          sm="6"
-        >
-          <CardStatisticsVertical v-bind="totalProfit" />
-        </VCol>
-
-        <VCol
-          cols="12"
-          sm="6"
-        >
-          <CardStatisticsVertical v-bind="newProject" />
-        </VCol>
-
-        <VCol
-          cols="12"
-          sm="6"
-        >
-          <AnalyticsBarCharts />
-        </VCol>
-      </VRow>
-    </VCol>
-
-    <VCol
-      cols="12"
-      md="4"
-    >
-      <AnalyticsSalesByCountries />
-    </VCol>
-
-    <VCol
-      cols="12"
-      md="8"
-    >
-      <AnalyticsDepositWithdraw />
-    </VCol>
-
-    <VCol cols="12">
-      <AnalyticsUserTable />
-    </VCol>
-  </VRow>
+    </VRow>
 </template>
---
+++ src/pages/productMini.vue
@@ -0,0 +1,24 @@
+<template>
+    <VCard>
+        <VImg
+          :src="pages5"
+          contain
+        />
+        <VCardItem>
+          <VCardTitle>{{productId}}</VCardTitle>
+        </VCardItem>
+        <VCardText>
+            남성용
+        </VCardText>
+    </VCard>
+</template>
+
+<script setup>
+import pages5 from '@images/pages/5.jpg';
+const props = defineProps({
+  productId: {
+    type: String,
+    required: true
+  }
+});
+</script>
